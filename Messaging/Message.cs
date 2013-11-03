184301074f8d32378d7cc1788f1ae3b7b3468e3e
David Gaddy <dgaddy@mit.edu>
2013-11-03 17:05:05 -0500

    Worked more on Refbox - compiles now.

--- Messaging/Message.cs
+++ Messaging/Message.cs
@@ -5,6 +5,10 @@ using System.Text;
 
 namespace Messaging
 {
+    /// <summary>
+    /// Subclass this to make a message that can be sent through service manager.
+    /// Subclasses should be immutable so they are safe to pass around.
+    /// </summary>
     public class Message
     {
     }
---
+++ Messaging/RefboxStateMessage.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Core;
+using Messaging;
+
+namespace RefBox
+{
+    public class RefboxStateMessage : Message
+    {
+        public Score Score
+        {
+            get;
+            private set;
+        }
+        readonly char command;
+        readonly char lastCommand;
+        PlayType GetPlayType(Team team) {
+            switch ((RefBoxMessageType)command)
+            {
+                case RefBoxMessageType.HALT:
+                    // stop bots completely
+                    return PlayType.Halt;
+                case RefBoxMessageType.START:
+                    return PlayType.NormalPlay;
+                case RefBoxMessageType.CANCEL:
+                case RefBoxMessageType.STOP:
+                case RefBoxMessageType.TIMEOUT_BLUE:
+                case RefBoxMessageType.TIMEOUT_YELLOW:
+                    //go to stopped/waiting state
+                    return PlayType.Stopped;
+                case RefBoxMessageType.TIMEOUT_END_BLUE:
+                case RefBoxMessageType.TIMEOUT_END_YELLOW:
+                case RefBoxMessageType.READY:
+                    if (((RefBoxMessageType)lastCommand == RefBoxMessageType.PENALTY_BLUE && team == Team.Blue) ||
+                        ((RefBoxMessageType)lastCommand == RefBoxMessageType.PENALTY_YELLOW && team == Team.Yellow))
+                        return PlayType.PenaltyKick_Ours;
+                    else if (((RefBoxMessageType)lastCommand == RefBoxMessageType.KICKOFF_BLUE && team == Team.Blue) ||
+                        ((RefBoxMessageType)lastCommand == RefBoxMessageType.KICKOFF_YELLOW && team == Team.Yellow))
+                        return PlayType.KickOff_Ours;
+                    else
+                        Console.WriteLine("ready state not expected, previous command: " + lastCommand);
+                    break;
+                case RefBoxMessageType.KICKOFF_BLUE:
+                    if (team == Team.Yellow)
+                        return PlayType.KickOff_Theirs;
+                    else
+                        return PlayType.KickOff_Ours_Setup;
+                case RefBoxMessageType.INDIRECT_BLUE:
+                case RefBoxMessageType.DIRECT_BLUE:
+                    if (team == Team.Yellow)
+                        return PlayType.SetPlay_Theirs;
+                    else
+                        return PlayType.SetPlay_Ours;
+                case RefBoxMessageType.KICKOFF_YELLOW:
+                    if (team == Team.Blue)
+                        return PlayType.KickOff_Theirs;
+                    else
+                        return PlayType.KickOff_Ours_Setup;
+                case RefBoxMessageType.INDIRECT_YELLOW:
+                case RefBoxMessageType.DIRECT_YELLOW:
+                    if (team == Team.Blue)
+                        return PlayType.SetPlay_Theirs;
+                    else
+                        return PlayType.SetPlay_Ours;
+                case RefBoxMessageType.PENALTY_BLUE:
+                    // handle penalty
+                    if (team == Team.Yellow)
+                        return PlayType.PenaltyKick_Theirs;
+                    else
+                        return PlayType.PenaltyKick_Ours_Setup;
+                case RefBoxMessageType.PENALTY_YELLOW:
+                    // penalty kick
+                    // handle penalty
+                    if (team == Team.Blue)
+                        return PlayType.PenaltyKick_Theirs;
+                    else
+                        return PlayType.PenaltyKick_Ours_Setup;
+            }
+            return PlayType.Halt;
+        }
+
+        public RefboxStateMessage(Score score, char command, char previousCommand)
+        {
+            this.Score = score;
+            this.command = command;
+            this.lastCommand = previousCommand;
+        }
+
+        public void LoadConstants()
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+
+    }
+}
