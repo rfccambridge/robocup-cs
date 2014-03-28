d28debf50fda3341ba9d92d993d1694745b3cb51
dgaddy <dgaddy@mit.edu>
2014-03-28 13:53:36 -0400

    Adding a function to evaluate offensiveness

---
+++ Strategy/HowOffensive.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RFC.Core;
+using RFC.Messaging;
+using RFC.Geometry;
+
+namespace RFC.Strategy
+
+{
+    class HowOffensive
+    {
+        Team Ourteam;
+        public HowOffensive(Team team)
+        {
+            Ourteam= team;
+        }
+
+        public float Evaluate(FieldVisionMessage msg)
+        {
+
+            List<RobotInfo> OurInfo = msg.GetRobots(Ourteam);
+            BallInfo ball = msg.Ball;
+            List<double> OurDistances = new List<double>();
+            foreach (RobotInfo info in OurInfo)
+            {
+                OurDistances.Add(info.Position.distance(ball.Position));
+            }
+            Team TheirTeam;
+            if (Ourteam == Team.Yellow)
+                TheirTeam = Team.Blue;
+            else
+                TheirTeam = Team.Yellow;
+            List<RobotInfo> TheirInfo = msg.GetRobots(Ourteam);
+            List<double> TheirDistances = new List<double>();
+            foreach (RobotInfo info in OurInfo)
+            {
+                
+                    TheirDistances.Add(info.Position.distance(ball.Position));
+            } 
+
+            return 0;
+            
+        }
+    }
+}
+
+
+                
+
+    }
+}
