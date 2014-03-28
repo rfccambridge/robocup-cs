91cf2aeef1ad4b2b4f1e1b1bc268b18b2ef4ced8
dgaddy <dgaddy@mit.edu>
2014-03-28 15:03:30 -0400

    Shot1.1

---
+++ Strategy/Shot1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RFC.Core;
+using RFC.Messaging;
+using RFC.Geometry;
+
+namespace RFC.Strategy
+{
+    class Shot1
+    {
+        Team team;
+
+        public Shot1 (Team team2){
+               team = team2;
+        }
+
+        public void evaluate (FieldVisionMessage fvm) {
+            List <RobotInfo> location = fvm.GetRobots();
+            foreach (RobotInfo i in location) {
+
+                Vector2 difference = i.Position - fvm.Ball.Position;
+            }
+
+        }
+
+    }
+}
