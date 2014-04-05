3b354f7df2d1d4d6d0e7ae073aa58e9abd9663ee
muherng <morrisyau@college.harvard.edu>
2014-04-05 13:21:33 -0700

    more shot

--- Strategy/Shot1.cs
+++ Strategy/Shot1.cs
@@ -26,7 +26,7 @@ namespace RFC.Strategy
             }
         }
 
-        public void evaluate (FieldVisionMessage fvm) {
+        public List <double> evaluate (FieldVisionMessage fvm) {
             List <RobotInfo> location = fvm.GetRobots();
             List <edge> intervals = new List <edge> ();
             for (int i = 0; i < location.Count(); i++){
@@ -37,6 +37,7 @@ namespace RFC.Strategy
                 intervals.Add(new edge(angle - sweep, false));
                 intervals.Add(new edge(angle + sweep, true)); 
             }
+            
             Vector2 goal1 = Constants.FieldPts.THEIR_GOAL_BOTTOM - fvm.Ball.Position;
             Vector2 goal2 = Constants.FieldPts.THEIR_GOAL_TOP - fvm.Ball.Position;
             double angle1 = goal1.cartesianAngle();
@@ -45,8 +46,35 @@ namespace RFC.Strategy
             intervals.Add(new edge(angle2, false));
             List <edge> Sort = intervals.OrderBy(o => o.d).ToList();
 
+            int status = 0;
+            List<edges> statuses = new List<edges>();
+            List <doubles> open_arc = new List <doubles> ();
+            for (int i = 0; i < Sort.Count(); i++){
+                if (status == 1){
+                    statuses.Add(i);
+                    open_arc.Add(statuses[i].d - statuses[i - 1].d);
+                }
+                if (i.b = true){
+                    status++;
+                } else {
+                    status--;
+                }
+                if (status == 1) statuses.Add(i);          
+            }
+
+            double maximum = 0;
+            int index = 0;
+            for (int i = 0; i < open_arc.Count(); i++)
+            {
+                if (open_arc[i] > maximum)
+                {
+                    maximum = open_arc[i];
+                    index = i;
+                }
+            }
 
 
+           return {(statuses[(index - 1) * 2 + 1].d + statuses[index * 2].d) / 2, open_arc[index]};
         }
 
     }
