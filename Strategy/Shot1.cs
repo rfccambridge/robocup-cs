e83dbb4b0971d6fab3e73882c9d822753f0be6d6
muherng <morrisyau@college.harvard.edu>
2014-04-05 13:38:40 -0700

    Recommit

--- Strategy/Shot1.cs
+++ Strategy/Shot1.cs
@@ -47,19 +47,19 @@ namespace RFC.Strategy
             List <edge> Sort = intervals.OrderBy(o => o.d).ToList();
 
             int status = 0;
-            List<edges> statuses = new List<edges>();
-            List <doubles> open_arc = new List <doubles> ();
+            List<edge> statuses = new List<edge>();
+            List <double> open_arc = new List <double> ();
             for (int i = 0; i < Sort.Count(); i++){
                 if (status == 1){
-                    statuses.Add(i);
+                    statuses.Add(Sort[i]);
                     open_arc.Add(statuses[i].d - statuses[i - 1].d);
                 }
-                if (i.b = true){
+                if (Sort[i].b){
                     status++;
                 } else {
                     status--;
                 }
-                if (status == 1) statuses.Add(i);          
+                if (status == 1) statuses.Add(Sort[i]);          
             }
 
             double maximum = 0;
@@ -73,8 +73,12 @@ namespace RFC.Strategy
                 }
             }
 
-
-           return {(statuses[(index - 1) * 2 + 1].d + statuses[index * 2].d) / 2, open_arc[index]};
+            List<double> shot_arc = new List<double>();
+           double shot = ((statuses[(index - 1) * 2 + 1].d + statuses[index * 2].d)/2.0);
+           double arc = open_arc[2*index];  
+           shot_arc.Add(shot);
+           shot_arc.Add(arc);
+           return shot_arc;
         }
 
     }
