24be66fc06cf729704f6c5fbf59b5630db86374e
Eric Lu <penlume@gmail.com>
2014-04-12 15:39:00 -0400

    simple goalie implementation tries to stay between ball and goal and moves to lead ball in motions

--- Strategy/Goalie.cs
+++ Strategy/Goalie.cs
@@ -11,14 +11,24 @@ namespace RFC.Strategy
     class Goalie
     {
         Team team;
+        int ID;
         public Goalie(Team team, int ID)
         {
             this.team = team;
+            this.ID = ID;
         }
         public RobotInfo getGoalie(FieldVisionMessage msg)
         {
-            List<RobotInfo> ours = msg.GetRobots(team);
-            return null;
+            BallInfo ball = msg.Ball;
+
+            // calculate ball position: simply project onto a circle surrounding the goal
+            Geometry.Vector2 projectedBallPosition = (ball.Position - Constants.FieldPts.OUR_GOAL).normalizeToLength(Constants.Field.GOAL_WIDTH/2) + Constants.FieldPts.OUR_GOAL;
+
+            // robot velocity to follow: reduce velocity based on distance to goal; project to circle tangent
+            Geometry.Vector2 reducedBallVelocity = ball.Velocity / ((ball.Position - Constants.FieldPts.OUR_GOAL).magnitude()/(Constants.Field.GOAL_WIDTH/2));
+            Geometry.Vector2 projectedBallVelocity = reducedBallVelocity.perpendicularComponent(ball.Position - Constants.FieldPts.OUR_GOAL);
+
+            return new RobotInfo(projectedBallPosition, projectedBallVelocity, 0, 0, ID);
         }
     }
 }
