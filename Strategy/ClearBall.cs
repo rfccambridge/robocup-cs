75ed7931236eb01493fc02fd7df0f5076a36b6a3
dgaddy <dgaddy@mit.edu>
2014-05-03 15:15:17 -0400

    ClearBall

---
+++ Strategy/ClearBall.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RFC.Core;
+using RFC.Messaging;
+using RFC.Geometry;
+
+namespace Strategy
+{
+    class ClearBall
+    {
+    }
+}
