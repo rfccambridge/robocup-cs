2347ada2d51414cef286f3da3094684cf088091f
Eric R. Anschuetz <ericanschuetz@college.harvard.edu>
2014-10-09 14:58:36 -0400

    Added empty TimeoutBehavior class.

---
+++ Strategy/TimeoutBehavior.cs
@@ -0,0 +1,54 @@
+﻿using RFC.Core;
+using RFC.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFC.Strategy
+{
+    public class TimeoutBehavior
+    {
+        private enum State { Timeout, Victory };
+        private DateTime startTime;
+        private Team team;
+        private int goalie_id;
+        private ServiceManager msngr;
+
+        private static readonly State STATE = State.Timeout;
+        // private static readonly State START_STATE = State.Victory;
+
+        public TimeoutBehavior(Team team, int goalie_id)
+        {
+            this.startTime = DateTime.Now;
+            this.team = team;
+            this.goalie_id = goalie_id;
+            this.msngr = ServiceManager.getServiceManager();
+        }
+
+        private void timeoutHandle(FieldVisionMessage fieldVision)
+        {
+            // TODO
+        }
+
+        private void victoryHandle(FieldVisionMessage fieldVision)
+        {
+            // TODO
+        }
+
+        public void Handle(FieldVisionMessage fieldVision)
+        {
+            switch (STATE)
+            {
+                case State.Timeout:
+                    timeoutHandle(fieldVision);
+                    break;
+
+                case State.Victory:
+                    victoryHandle(fieldVision);
+                    break;
+            }
+        }
+    }
+}
