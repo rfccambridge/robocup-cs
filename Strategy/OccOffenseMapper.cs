4fa1d14731732d616c59c8c879cdd644f62e5071
Eric Wieser <wieser.eric@gmail.com>
2015-09-26 16:44:42 -0400

    Use new lattice class in OccOffenseMapper.
    Class boundaries still cast to arrays

--- Strategy/OccOffenseMapper.cs
+++ Strategy/OccOffenseMapper.cs
@@ -90,8 +90,6 @@ namespace RFC.Strategy
             this.data = new T[spec.Samples, spec.Samples];
         }
 
-
-
         public T this[Vector2 pos]
         {
             get
@@ -137,30 +135,24 @@ namespace RFC.Strategy
         // how important is good line of sight to the goal for our other robots?
         public const double SHOT_EXP = 2.0;
         Team team;
-
-        private double LAT_HSTART;
-        private static readonly double LAT_VSTART = Constants.Field.YMIN;
-
-        private double LAT_HEND;
-        private static readonly double LAT_VEND = Constants.Field.YMAX;
-
-        private double LAT_HSIZE;
-        private double LAT_VSIZE;
-
+        
+        private LatticeSpec latticeSpec;
+        private Lattice<double> shotLattice;
+  
         private ServiceManager msngr;
-
-        private double[,] shotMap = new double[LAT_NUM, LAT_NUM];
-
+        
         public OccOffenseMapper(Team team)
         {
             this.team = team;
             msngr = ServiceManager.getServiceManager();
 
-            // setting field size
-            this.LAT_HSTART = Constants.Field.XMIN / 2;
-            this.LAT_HEND = Constants.Field.XMAX;
-            LAT_HSIZE = (LAT_HEND - LAT_HSTART) / LAT_NUM;
-            LAT_VSIZE = (LAT_VEND - LAT_VSTART) / LAT_NUM;
+            this.latticeSpec = new LatticeSpec(
+                new Geometry.Rectangle(
+                    Constants.Field.XMIN, Constants.Field.XMAX,
+                    Constants.Field.YMIN, Constants.Field.YMAX
+                ),
+                LAT_NUM
+            );
         }
 
         public OccOffenseMapper(Team team, double xmin, double xmax)
@@ -168,21 +160,27 @@ namespace RFC.Strategy
             this.team = team;
             msngr = ServiceManager.getServiceManager();
 
-            // setting field size
-            this.LAT_HSTART = xmin;
-            this.LAT_HEND = xmax;
-            LAT_HSIZE = (LAT_HEND - LAT_HSTART) / LAT_NUM;
-            LAT_VSIZE = (LAT_VEND - LAT_VSTART) / LAT_NUM;
+            this.latticeSpec = new LatticeSpec(
+                new Geometry.Rectangle(
+                    xmin, xmax,
+                    Constants.Field.YMIN, Constants.Field.YMAX
+                ),
+                LAT_NUM
+            );
         }
 
+        [Obsolete]
         public int[] vecToInd(Vector2 v)
         {
-            return new int[] {(int)Math.Round((v.X - LAT_HSTART) / LAT_HSIZE), (int)Math.Round((v.Y - LAT_VSTART) / LAT_VSIZE)};
+            int x, y;
+            latticeSpec.vectorToIndex(v, out x, out y);
+            return new int[] { x, y };
         }
 
+        [Obsolete]
         public Vector2 indToVec(int i, int j)
         {
-            return new Vector2(i * LAT_HSIZE + LAT_HSTART, j * LAT_VSIZE + LAT_VSTART);
+            return latticeSpec.indexToVector(i, j);
         }
 
         public static void printDoubleMatrix(double[,] a)
@@ -207,62 +205,44 @@ namespace RFC.Strategy
         // calculate how good a position is for making a goal shot
         public void update(List<RobotInfo> ourTeam, List<RobotInfo> theirTeam, BallInfo ball, FieldVisionMessage fmsg)
         {
-            for (double x = LAT_HSTART; x < LAT_HEND; x += LAT_HSIZE)
-            {
-                for (double y = LAT_VSTART; y < LAT_VEND; y += LAT_VSIZE)
+            shotLattice = latticeSpec.Create(pos => {
+                // not needed since we now have ShotOpportunity
+                /*
+                // find angle of opening for position
+                Vector2 vecBotGoal = Constants.FieldPts.THEIR_GOAL_BOTTOM - pos;
+                Vector2 vecTopGoal = Constants.FieldPts.THEIR_GOAL_TOP - pos;
+                double goalAngle = Math.Acos(vecBotGoal.cosineAngleWith(vecTopGoal));
+
+                // iterate through other robots, adding distance between (line of sight between position and goal) and other robot
+                Vector2 vecCentGoal = pos - Constants.FieldPts.THEIR_GOAL;
+                double distSum = 1;
+                foreach (RobotInfo rob in theirTeam)
                 {
-                    
-                    Vector2 pos = new Vector2(x, y);
-                    // not needed since we now have ShotOpportunity
-                    /*
-                    // find angle of opening for position
-                    Vector2 vecBotGoal = Constants.FieldPts.THEIR_GOAL_BOTTOM - pos;
-                    Vector2 vecTopGoal = Constants.FieldPts.THEIR_GOAL_TOP - pos;
-                    double goalAngle = Math.Acos(vecBotGoal.cosineAngleWith(vecTopGoal));
 
-                    // iterate through other robots, adding distance between (line of sight between position and goal) and other robot
-                    Vector2 vecCentGoal = pos - Constants.FieldPts.THEIR_GOAL;
-                    double distSum = 1;
-                    foreach (RobotInfo rob in theirTeam)
+                    double dist = (rob.Position - pos).perpendicularComponent(vecCentGoal).magnitude();
+                    if (dist < IGN_THRESH && ((vecCentGoal - rob.Position).magnitude() > (vecCentGoal - pos).magnitude()))
                     {
-                        
-                        double dist = (rob.Position - pos).perpendicularComponent(vecCentGoal).magnitude();
-                        if (dist < IGN_THRESH && ((vecCentGoal - rob.Position).magnitude() > (vecCentGoal - pos).magnitude()))
-                        {
-                            distSum -= 1 * Math.Exp(-dist);
-                        }
+                        distSum -= 1 * Math.Exp(-dist);
                     }
-
-                    if (distSum < 0)
-                        distSum = 0;
-                    */
-                    int[] ind = vecToInd(new Vector2(x, y));
-                    int i = ind[0];
-                    int j = ind[1];
-                    // shouldn't happen but just to be safe
-                    if (i > LAT_NUM - 1)
-                    {
-                        i = LAT_NUM - 1;
-                    }
-                    if (j > LAT_NUM - 1)
-                    {
-                        j = LAT_NUM - 1;
-                    }
-                    // make nonlinear (put in threshold)
-                    // subtract (so that number of robots doesn't factor in)
-                    // shotMap[i, j] = normalize(goalAngle * distSum);
-                    
-                    ShotOpportunity shot = Shot1.evaluate(fmsg, team, pos);
-                    shotMap[i, j] = shot.arc;
                 }
-            }
+
+                if (distSum < 0)
+                    distSum = 0;
+                */
+
+                // make nonlinear (put in threshold)
+                // subtract (so that number of robots doesn't factor in)
+                //]return normalize(goalAngle * distSum);
+                ShotOpportunity shot = Shot1.evaluate(fmsg, team, pos);
+                return shot.arc;
+            });
         }
 
         // dribble to better place--doesn't depend on bounce angle
         // should only depend on how good shot is?
         public double[,] getDrib(List<RobotInfo> ourTeam, List<RobotInfo> theirTeam, BallInfo ball)
         {
-            return shotMap;
+            return (double[,]) shotLattice;
             // distance from ball to pos?
         }
 
@@ -271,96 +251,77 @@ namespace RFC.Strategy
         // make sure position has good shot
         public double[,] getPass(List<RobotInfo> ourTeam, List<RobotInfo> theirTeam, BallInfo ball, FieldVisionMessage fmsg)
         {
-            double[,] map = new double[LAT_NUM, LAT_NUM];
-            for (double x = LAT_HSTART; x < LAT_HEND; x += LAT_HSIZE)
+            return (double[,]) latticeSpec.Create(pos =>
             {
-                for (double y = LAT_VSTART; y < LAT_VEND; y += LAT_VSIZE)
+                Vector2 vecToBall = ball.Position - pos;
+                Vector2 vecToGoal = Constants.FieldPts.THEIR_GOAL - pos;
+
+
+                // see if position has good line of sight with ball
+                double distSum = 1.0;
+                double tooClose = 1.0;
+
+                foreach (RobotInfo rob in theirTeam)
                 {
-                    Vector2 pos = new Vector2(x, y);
-                    Vector2 vecToBall = ball.Position - pos;
-                    Vector2 vecToGoal = Constants.FieldPts.THEIR_GOAL - pos;
 
-                    
-                    // see if position has good line of sight with ball
-                    double distSum = 1.0;
-                    double tooClose = 1.0;
-                    
-                    foreach (RobotInfo rob in theirTeam)
+                    Vector2 dist = (rob.Position - pos);
+                    double perpdist = dist.perpendicularComponent(vecToBall).magnitude();
+                    if (perpdist < IGN_THRESH && (ball.Position - rob.Position).cosineAngleWith(vecToBall) > 0 && Vector2.dotproduct(pos, ball.Position, rob.Position) > Vector2.dotproduct(rob.Position, ball.Position, rob.Position))
                     {
-
-                        Vector2 dist = (rob.Position - pos);
-                        double perpdist = dist.perpendicularComponent(vecToBall).magnitude();
-                        if (perpdist < IGN_THRESH && (ball.Position - rob.Position).cosineAngleWith(vecToBall) > 0 && Vector2.dotproduct(pos,ball.Position,rob.Position) > Vector2.dotproduct(rob.Position,ball.Position, rob.Position))
-                        {
-                            distSum -= 1 * Math.Exp(-perpdist);
-                        }
-
-                        // also checking if too close
-                        if (dist.magnitude() < Constants.Basic.ROBOT_RADIUS * 4)
-                            tooClose = 0.0;
-
-                    }
-                    if (distSum < 0)
-                    {
-                        distSum = 0;
-                    }
-                    
-                    // account for distance to ball
-                    double distScore = Math.Atan2(1,vecToBall.magnitude());
-                    if (Constants.Basic.ROBOT_RADIUS * 4 > vecToBall.magnitude())
-                        distScore = 0;
-
-                    // account for distance to our robots
-                    double minRobotDist = 100.0;
-                    foreach (RobotInfo rob in ourTeam)
-                    {
-                        double robotDist = (rob.Position - pos).magnitude();
-                        if (robotDist < minRobotDist)
-                            minRobotDist = robotDist;
-                    }
-                    double robDistScore = Math.Exp(-minRobotDist);
-
-                    // calculate bounce score
-                    // make .5(1+cos)
-                    double currentBounceAngle = 180 * Math.Acos(vecToBall.cosineAngleWith(vecToGoal)) / Math.PI;
-                    if (double.IsNaN(currentBounceAngle))
-                        currentBounceAngle = 0;
-                    double bounceScore = 180 - currentBounceAngle;
-                    double worstBounceScore = 90 - Math.Abs(BOUNCE_ANGLE - 90);
-                    // if bounce score is worse than what robot can handle then position is pretty crappy
-                    if (bounceScore < worstBounceScore)
-                    {
-                        bounceScore = 0;
+                        distSum -= 1 * Math.Exp(-perpdist);
                     }
 
-                    int[] ind = vecToInd(new Vector2(x, y));
-                    int i = ind[0];
-                    int j = ind[1];
-                    // shouldn't happen but just to be safe
-                    if (i > LAT_NUM - 1)
-                    {
-                        i = LAT_NUM - 1;
-                    }
-                    if (j > LAT_NUM - 1)
-                    {
-                        j = LAT_NUM - 1;
-                    }
+                    // also checking if too close
+                    if (dist.magnitude() < Constants.Basic.ROBOT_RADIUS * 4)
+                        tooClose = 0.0;
 
-                    double isValid = 0;
-                    if (Avoider.isValid(pos, false))
-                        isValid = 1;
-
-                    map[i, j] = normalize(Math.Pow(shotMap[i, j], SHOT_EXP) * bounceScore * distSum * distScore * tooClose * isValid * robDistScore);
-
-                    
-
-                    //Console.WriteLine("result: " + normalize(shotMap[i, j] * bounceScore * distSum * distScore));
-                    //ShotOpportunity shot = Shot1.evaluatePosition(fmsg, pos, team);
-                    //map[i, j] = normalize(shotMap[i, j] * bounceScore * shot.arc);
-                    //msngr.vdb(new Vector2(x,y), Utilities.ColorUtils.numToColor(map[i,j],0,20));
                 }
-            }
-            return map;
+                if (distSum < 0)
+                {
+                    distSum = 0;
+                }
+
+                // account for distance to ball
+                double distScore = Math.Atan2(1, vecToBall.magnitude());
+                if (Constants.Basic.ROBOT_RADIUS * 4 > vecToBall.magnitude())
+                    distScore = 0;
+
+                // account for distance to our robots
+                double minRobotDist = 100.0;
+                foreach (RobotInfo rob in ourTeam)
+                {
+                    double robotDist = (rob.Position - pos).magnitude();
+                    if (robotDist < minRobotDist)
+                        minRobotDist = robotDist;
+                }
+                double robDistScore = Math.Exp(-minRobotDist);
+
+                // calculate bounce score
+                // make .5(1+cos)
+                double currentBounceAngle = 180 * Math.Acos(vecToBall.cosineAngleWith(vecToGoal)) / Math.PI;
+                if (double.IsNaN(currentBounceAngle))
+                    currentBounceAngle = 0;
+                double bounceScore = 180 - currentBounceAngle;
+                double worstBounceScore = 90 - Math.Abs(BOUNCE_ANGLE - 90);
+                // if bounce score is worse than what robot can handle then position is pretty crappy
+                if (bounceScore < worstBounceScore)
+                {
+                    bounceScore = 0;
+                }
+
+                double isValid = 0;
+                if (Avoider.isValid(pos, false))
+                    isValid = 1;
+
+                return normalize(Math.Pow(shotLattice[pos], SHOT_EXP) * bounceScore * distSum * distScore * tooClose * isValid * robDistScore);
+
+
+
+                //Console.WriteLine("result: " + normalize(shotMap[i, j] * bounceScore * distSum * distScore));
+                //ShotOpportunity shot = Shot1.evaluatePosition(fmsg, pos, team);
+                //map[i, j] = normalize(shotMap[i, j] * bounceScore * shot.arc);
+                //msngr.vdb(new Vector2(x,y), Utilities.ColorUtils.numToColor(map[i,j],0,20));
+            });
         }
 
         // used for debugging drawn maps
