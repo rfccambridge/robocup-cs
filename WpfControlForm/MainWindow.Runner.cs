37d20e7e376737ae7c75b481c24a1a45bdc8fec7
Eric Wieser <wieser.eric@gmail.com>
2015-10-24 16:52:38 -0400

    Add a new control form written in WPF

---
+++ WpfControlForm/MainWindow.Runner.cs
@@ -0,0 +1,113 @@
+﻿using RFC.Commands;
+using RFC.Core;
+using RFC.InterProcessMessaging;
+using RFC.Logging;
+using RFC.Messaging;
+using RFC.PathPlanning;
+using RFC.RefBox;
+using RFC.Strategy;
+using RFC.FieldDrawer;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using RFC.Vision;
+
+namespace RFC.WpfControlForm
+{
+    public partial class MainWindow
+    {
+        FieldDrawer.FieldDrawer fd = null;
+
+        public bool Run()
+        {
+            if (Running) return false;
+            
+            int maxRobotId = 12;
+
+            if (SelectedConnection == "Simulator")
+            {
+                try
+                {
+                    new SimulatorSender();
+                }
+                catch (ConnectionRefusedException)
+                {
+                    MessageBox.Show("Is the simulator running?", "Could not connect");
+                    return false;
+                }
+            }
+            else
+            {
+                try
+                {
+                    new SerialSender(SelectedConnection);
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show("Could not open given serial port.", "Could not connect");
+                    return false;
+                }
+            }
+
+            if (fd == null)
+            {
+                Vision.Vision vision = new Vision.Vision();
+
+                vision.Connect("224.5.23.2", 10002);
+                vision.Start();
+
+                new AveragingPredictor();
+
+                fd = new FieldDrawer.FieldDrawer();
+                fd.Show();
+            }
+
+            new LogHandler();
+
+            new SmoothRRTPlanner(true, maxRobotId, GoalieId);
+            new VelocityDriver();
+            new KickPlanner();
+            new BreakBeamEmulator(SelectedTeam);
+            //new DribblerControler(team);
+
+
+            new NormalTester(SelectedTeam, GoalieId, fd);
+            //new AlexTest(team, goalieNumber);
+            //new MovementTest(team);
+            //new OffenseTester(team, goalieNumber);
+            //new SetupTest(team, goalieNumber);
+            //new GoalieTest(team, goalieNumber);
+            //new SetupTest1(team, goalieNumber);
+            //new PlaySwitcher(team, goalieNumber, fd);
+            //new SetupTest(team, goalieNumber);
+            //new KickTester(team, goalieNumber);
+            //new TimeoutBehavior(team, goalieNumber);
+            //new PenaltyKickTester(team, goalieNumber);
+            //new KickOffBehaviorTester(team, goalieNumber);
+
+            MulticastRefBoxListener refbox = new MulticastRefBoxListener(SelectedTeam);
+
+            refbox.Connect("224.5.23.1", 10001);
+            refbox.Start();
+            ServiceManager.getServiceManager().SendMessage(new LogMessage("started"));
+
+            return true;
+        }
+
+        private void runButton_Click(object sender, RoutedEventArgs e)
+        {
+            Running = Run();
+        }
+
+        private void stopButton_Click(object sender, RoutedEventArgs e)
+        {
+            ServiceManager.getServiceManager().SendMessage(new StopMessage());
+            Running = false;
+        }
+    }
+
+}
---
+++ WpfControlForm/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using RFC.Core;
+using RFC.Messaging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RFC.WpfControlForm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Teams = Enum.GetValues(typeof(Team))
+                .Cast<Team>()
+                .Select(e => new KeyValuePair<Team, string>(e, e.ToString()))
+                .ToList();
+
+            Connections = SerialPort.GetPortNames().ToList();
+            SelectedConnection = "Simulator";
+            Connections.Insert(0, SelectedConnection);
+
+            DataContext = this;
+
+            SelectedTeam = Team.Yellow;
+        }
+
+        public Team SelectedTeam { get; set; }
+        public List<KeyValuePair<Team, string>> Teams { get; }
+
+        public string SelectedConnection { get; set; }
+        public List<string> Connections { get; }
+
+        public bool Flipped { get; set; }
+
+        public int GoalieId { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private bool _running;
+        public bool Running {
+            get { return _running; }
+            set {
+                _running = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Running)));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Stopped)));
+            }
+        }
+        public bool Stopped => !Running;
+    }
+}
